29471b389b0b67e27c967343509f17a0d07dd123
Santiago lezica <slezica89@gmail.com>
2010-10-13 15:05:52 -0300

    funcion para mostrar productos

--- WebContent/files/js/page.js
+++ WebContent/files/js/page.js
@@ -1,6 +1,9 @@
 // Page load script for ShopShark.
 
-var SECTIONS = ['main', 'searchr']
+var SECTIONS = { 'main' : 'main.html',
+                 'searchr' : 'searchr.html',
+                 'google' : 'http://www.google.com'
+               }
 
 $(document).ready(function() {
     	 $('#sidebar').load('sidebar.html');
@@ -10,5 +13,31 @@ $(document).ready(function() {
 function show(section) {
     if (!$.inArray(section, SECTIONS))
         section = 'main'
-    $('#content').load(section + '.html');
+    $('#content').load(SECTIONS[section]);
+}
+
+function testShowProducts() {
+    var array = [ { 'pic' : 'files/images/logo.png',
+                    'title' : 'Logo de ShopShark',
+                    'price' : 10000,
+                    'rank' : 10 } ];
+
+    showProducts(array);
+}
+
+function showProducts(array) {
+    var template = $('#item-template').html();
+    template = template.substring(4, template.length - 3);
+    
+    for (var i = 0; i < array.length; i++) {
+        var item = array[i];
+        
+        var item_html = template.replace(
+                        '%PIC', item.pic).replace(
+                        '%TITLE', item.title).replace(
+                        '%PRICE', item.price).replace(
+                        '%RANK', item.rank);
+
+        $('#list-body').append(item_html);
+    }
 }
